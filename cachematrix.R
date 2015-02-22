9b6e3bfc2e1e55df856ade305bb4d7aac9d185c7
AndrewBagley <AndrewBagley@users.noreply.github.com>
2015-02-22 11:37:52 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Put comments here that give an overall description of what your
+## Assignment: Caching the Inverse of a Matrix
 ## functions do
 
 ## Write a short comment describing this function
